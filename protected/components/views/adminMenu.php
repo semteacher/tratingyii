10efe2792816b6ac05f572d312c64a0c995799ab
semteacher <semteacher@gmail.com>
2014-07-13 09:18:52 +0300

    update adminmenu

--- protected/components/views/adminMenu.php
+++ protected/components/views/adminMenu.php
@@ -11,5 +11,6 @@
     <li><?php echo CHtml::link('Manage Indices',array('indices/admin')) . ' (' . Indices::model()->count() . ')'; ?></li>
     <li><?php echo CHtml::link('Manage Users',array('teachers/admin')) . ' (' . Teachers::model()->count() . ')'; ?></li>
     <li><?php echo CHtml::link('Manage Departments',array('departments/admin')) . ' (' . Departments::model()->count() . ')'; ?></li>
+    <li><?php echo CHtml::link('Manage User Assignments',array('teachers2departments/admin')) . ' (' . Teachers2departments::model()->count() . ')'; ?></li>
     <li><?php echo CHtml::link('Logout',array('site/logout')); ?></li>
 </ul>
